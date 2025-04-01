33e144b0cbd452f709d69bda14eb96e834b5f5b7
chenjiajun <feifeichen1999@gmail.com>
2025-04-01 13:47:39 +0800

    queryGames associated tag,GameTag table

--- services/gameService.js
+++ services/gameService.js
@@ -1,4 +1,4 @@
-const { Games } = require('../models');
+const { Games, GameTag, Tag } = require('../models');
 const { Op } = require('sequelize');
 const addGame = async (gameData) => {
   // 检查游戏是否已存在
@@ -12,27 +12,53 @@ const addGame = async (gameData) => {
   return game;
 };
 
-
-
 const queryGames = async ({ game_name }) => {
   // 构建查询条件
   const where = {};
   if (game_name) {
     where.game_name = {
-      [Op.like]: `%${game_name}%` // 模糊查询游戏名称
+      [Op.like]: `%${game_name}%`, // 模糊查询游戏名称
     };
   }
 
-  // 查询所有符合条件的游戏
+  // 查询所有符合条件的游戏，包含GameTag和Tag表数据
   const games = await Games.findAll({
     where,
-    order: [['createdAt', 'DESC']] // 按创建时间倒序
+    include: [
+      {
+        model: GameTag,
+        as: 'gameTagRelations',
+        attributes: ['game_id', 'tag_id'],
+        include: [
+          {
+            model: Tag,
+            as: 'gameTagRelation',
+            attributes: ['tag_id', 'tag_name', 'tag_type'],
+          },
+        ],
+      },
+    ],
+    order: [['createdAt', 'DESC']], // 按创建时间倒序
   });
 
-  return games;
+  // 优化返回数据格式
+  return games.map((game) => {
+    const gameData = game.toJSON();
+    return {
+      ...gameData,
+      ranks: gameData.rank_data?.ranks || [],
+      tags: (gameData.gameTagRelations || []).map((relation) => ({
+        id: relation.gameTagRelation.tag_id,
+        name: relation.gameTagRelation.tag_name,
+        type: relation.gameTagRelation.tag_type,
+      })),
+      gameTagRelations: undefined,
+      rank_data: undefined,
+    };
+  });
 };
 
 module.exports = {
   addGame,
-  queryGames
+  queryGames,
 };
